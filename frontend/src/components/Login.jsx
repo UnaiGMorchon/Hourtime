e395c781f84b52406d32f1707cb6749e3d3fce02
UnaiGMorchon <unaitxu79@gmail.com>
2023-05-26 15:36:12 +0200

    login css ok botones home pero sigup falta

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -44,47 +44,42 @@ const Login = () => {
 
   return (
     <>
-      <main>
-        <section>
-          <div>
-            <p> FocusApp</p>
-            <form>
-              <div>
-                <label htmlFor='email-address'>Email address</label>
-                <input
-                  id='email-address'
-                  name='email'
-                  type='email'
-                  required
-                  placeholder='Email address'
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-              </div>
-
-              <div>
-                <label htmlFor='password'>Password</label>
-                <input
-                  id='password'
-                  name='password'
-                  type='password'
-                  required
-                  placeholder='Password'
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-              </div>
-
-              <div>
-                <button onClick={onLogin}>Login</button>
-                <button onClick={onGoogleLogin}>Login with google</button>
-              </div>
-            </form>
-
-            <p className='text-sm text-white text-center'>
-              No account yet? <NavLink to='/signup'>Sign up</NavLink>
-            </p>
+      <div className='formulario'>
+        <form>
+          <div className='formularioemail'>
+            <label htmlFor='email-address'>Email address</label>
+            <input
+              id='email-address'
+              name='email'
+              type='email'
+              required
+              placeholder='Email address'
+              onChange={(e) => setEmail(e.target.value)}
+            />
           </div>
-        </section>
-      </main>
+
+          <div className='formulariopassword'>
+            <label htmlFor='password'>Password</label>
+            <input
+              id='password'
+              name='password'
+              type='password'
+              required
+              placeholder='Password'
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+
+          <div className='formulariobotones'>
+            <button onClick={onLogin}>Login</button>
+            <button onClick={onGoogleLogin}>Login with google</button>
+          </div>
+        </form>
+
+        <p className='text-sm text-white text-center'>
+          No account yet? <NavLink to='/signup'>Sign up</NavLink>
+        </p>
+      </div>
     </>
   );
 };
